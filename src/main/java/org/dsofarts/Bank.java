f4ea81d33f7f194acce754d8c5f139fe5e705df6
”Egor <dronovegorvl@yandex.com>
2022-09-24 22:04:35 +0300

    Create User constructor

--- src/main/java/org/dsofarts/Bank.java
+++ src/main/java/org/dsofarts/Bank.java
@@ -6,4 +6,9 @@ public class Bank {
     private String name;
     private ArrayList<User> users;
     private ArrayList<Account> accounts;
+
+    public String getNewUserUUID() {
+
+    }
+
 }
--- src/main/java/org/dsofarts/User.java
+++ src/main/java/org/dsofarts/User.java
@@ -1,6 +1,8 @@
 package org.dsofarts;
 
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.security.MessageDigest;
 
 public class User {
     private String firstName;
@@ -9,4 +11,40 @@ public class User {
     private String uuid;
     private byte pinHash[];
     private ArrayList<Account> accounts;
+
+    /**
+     * Create a new user
+     * @param firstName
+     * @param middleName
+     * @param lastName
+     * @param pin
+     * @param theBank
+     */
+    public User(String firstName, String middleName, String lastName, String pin, Bank theBank) {
+
+        // set user's name
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+
+        // store the pin's MD5 hash, rather than the original value for security reason
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            this.pinHash = md.digest(pin.getBytes());
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("error, caught NoSuchAlgorithmException");
+            e.printStackTrace();
+            System.exit(1);
+        }
+
+        // get a new, unique universal ID for the user
+        this.uuid= theBank.getNewUserUUID();
+
+        // create empty list of accounts
+        this.accounts = new ArrayList<Account>();
+
+        //print log message
+        System.out.printf("New user %s, %s, %s width ID %s created. \n", lastName, middleName, firstName, this.uuid);
+
+    }
 }
